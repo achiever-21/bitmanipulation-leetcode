b79f6d74e392b6d30f51fd23ea112e0cb728f0f8
Bhavya sri Polishetti <132044694+achiever-21@users.noreply.github.com>
2023-10-19 00:25:23 +0530

    Update binary addition.py

--- binary addition.py
+++ binary addition.py
@@ -1,3 +1,17 @@
+**INTUTION:
+-----------------------------------
+<br> binary addition </br>
+ 1+1=1,c=1
+1+0=1 ,c=0
+0+1=1,c=0
+<h1>we can see that input in  string format to perform addition on this input the length of two numbers should be same .</h1>
+<h1> so we first take each right most digit from two strings a,b and check above condition .<h1>
+<h1> becoz of diff length of strings </h1>
+<h1> we will use a while loop where condition length a or length b </h1>
+<h1> individually we will check rightmost bit of each input is 1 or not </h1>
+<h1> after checking both x and y we will update our ans and carry too</h1>
+<h1> updation of ans=(x+y+c)%2 and c=(x+y+c)//2<h1>
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         i=len(a)-1
